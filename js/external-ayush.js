ad5ba41c2112078caf6f14689ee266aae126d750
DevAyushMittal <52674089+DevAyushMittal@users.noreply.github.com>
2019-09-12 23:11:41 -0700

    code arranged properly

--- js/external-ayush.js
+++ js/external-ayush.js
@@ -17,3 +17,76 @@ $('.carousel[data-type="multi"] .item').each(function() {
 
 
 // multiple-item end 
+
+
+// scroll animation 
+
+
+$(document).ready(function(){
+	// Add smooth scrolling to all links
+	$("a").on('click', function(event) {
+  
+	  // Make sure this.hash has a value before overriding default behavior
+	  if (this.hash !== "") {
+		// Prevent default anchor click behavior
+		event.preventDefault();
+  
+		// Store hash
+		var hash = this.hash;
+  
+		// Using jQuery's animate() method to add smooth page scroll
+		// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+		$('html, body').animate({
+		  scrollTop: $(hash).offset().top
+		}, 800, function(){
+	 
+		  // Add hash (#) to URL when done scrolling (default click behavior)
+		  window.location.hash = hash;
+		});
+	  } // End if
+	});
+  });
+
+
+//   end 
+
+
+
+// modal 
+
+
+$(document).ready(function(){
+    $(".modalactive2").click(function(){
+		$("#pcontactmodal").show(500);
+	});
+   
+ 
+});
+
+$(document).ready(function(){
+    $(".close").click(function(){
+		$("#pcontactmodal").fadeOut(1000);
+	});
+   
+ 
+});
+
+
+$(document).ready(function(){
+    $(".modalactive").click(function(){
+		$(".contactmodal").show(500);
+	});
+   
+ 
+});
+
+$(document).ready(function(){
+    $(".close").click(function(){
+		$(".contactmodal").fadeOut(1000);
+	});
+   
+ 
+});
+
+
+// end 
