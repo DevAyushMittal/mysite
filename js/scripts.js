8ab6230cd9e1ff82b21caedf8f6f4ec24cedb1fb
DevAyushMittal <52674089+DevAyushMittal@users.noreply.github.com>
2020-05-12 16:45:46 +0530

    changes made

--- js/scripts.js
+++ js/scripts.js
@@ -30,6 +30,40 @@
 
 */
 
+jQuery("#carousel").owlCarousel({
+    autoplay: true,
+    lazyLoad: true,
+    loop: true,
+    margin: 20,
+     /*
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+  
+      600: {
+        items: 3
+      },
+  
+      1024: {
+        items: 4
+      },
+  
+      1366: {
+        items: 4
+      }
+    }
+  });
+
+
 
 (function ($) {
     'use strict';
@@ -224,3 +258,4 @@
             });
 
    })(jQuery);
+   
