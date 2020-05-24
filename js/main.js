b8c44d288c18cd9d924e80dbba31fdae16dfc7ce
devayush <imdeveloperayush@gmail.com>
2020-05-24 11:30:27 +0530

    fixed layout structure

--- js/main.js
+++ js/main.js
@@ -40,3 +40,44 @@ var galleryTop = new Swiper('.swiper-container.testimonial', {
 	  }
   });
   
+
+
+  $('.counting').each(function() {
+	var $this = $(this),
+		countTo = $this.attr('data-count');
+	
+	$({ countNum: $this.text()}).animate({
+	  countNum: countTo
+	},
+  
+	{
+  
+	  duration: 30000,
+	  easing:'linear',
+	  step: function() {
+		$this.text(Math.floor(this.countNum));
+	  },
+	  complete: function() {
+		$this.text(this.countNum);
+		//alert('finished');
+	  }
+  
+	});  
+	
+  
+  });
+  
+//   talk js 
+	
+	var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+	(function(){
+	var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+	s1.async=true;
+	s1.src='https://embed.tawk.to/5ec615276f7d401ccbb8895f/default';
+	s1.charset='UTF-8';
+	s1.setAttribute('crossorigin','*');
+	s0.parentNode.insertBefore(s1,s0);
+	})();
+
+
+	
